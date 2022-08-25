018106f14b255ee9fa99884869d4ae9db8d03bab
aakarsh604 <aakarsh604@gmail.com>
2022-08-25 23:11:55 +0530

    created productdetails page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,8 @@
-import Navbar from "./components/Navbar";
-import Products from "./components/products/Products";
+
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Products />
     </div>
   );
 }
