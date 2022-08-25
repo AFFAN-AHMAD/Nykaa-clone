1f2ecb04b814e601b399181b561761cdbf829b13
skashyap061998 <skashyap061998@gmail.com>
2022-08-25 10:48:45 +0530

    day1 part1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      
+      <Navbar/>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from "./Navbar.module.css"
+import {Link} from "react-router-dom"
+const Navbar = () => {
+  return (
+    <div>
+        <div className={styles.Sc1}>
+<img src="https://theme.zdassets.com/theme_assets/222949/60503496e109d9e249ab19791fb335f0459a483e.png" alt="" />
+<div className={styles.sc11}>
+<Link to=''>Categories</Link>
+<Link to=''>Brands</Link>
+<Link to=''>Luxe</Link>
+<Link to=''>Nykaa Fashion</Link>
+<Link to=''>Beauty Advice</Link>
+</div>
+<div className={styles.sc12}>
+    <input type="text" placeholder='ase hi'/>
+</div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
