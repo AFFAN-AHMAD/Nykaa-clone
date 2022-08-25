098606e9bc27c401e7ad8b4e313fc467f14d7a72
Aman Singh <aman.iit96@gmail.com>
2022-08-25 23:01:32 +0530

    day3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,10 @@
-<<<<<<< Updated upstream
-import Navbar from "./components/Navbar";
-=======
-import LandingPage from "./pages/LandingPage/LandingPage";
+// import Navbar from "./components/Navbar";
+// import LandingPage from "./pages/LandingPage/LandingPage";
 
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div>
-<<<<<<< Updated upstream
-      <Navbar/>
-=======
-      <LandingPage/>
->>>>>>> Stashed changes
     </div>
   );
 }
