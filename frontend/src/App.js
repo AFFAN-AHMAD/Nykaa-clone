168607d419e6d05e33a1d70c99817f36c8ab437c
skashyap061998 <skashyap061998@gmail.com>
2022-08-27 17:00:53 +0530

    day4 done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Otp from "./pages/otp/Otp";
+import Signup from "./pages/Signup/Signup";
 
 
 function App() {
   return (
   <div>
 
+<Routes>
+<Route path="/" element={<Navbar/>}/>
+
+  <Route path="/signup" element={<Signup/>}/>
+  <Route path="/otp"  element={<Otp/>}/>
+</Routes>
   </div>
   )
 }
