9862f82ae423bb6d620dd9d7f08a35083c90230c
aakarsh604 <aakarsh604@gmail.com>
2022-08-27 17:42:54 +0530

    added backend for product details page and cart page

---
+++ backend/routes/cart.routes.js
@@ -0,0 +1,39 @@
+const { Router } = require("express");
+const CartModel = require("../models/Cart.model");
+
+const cartRouter = Router();
+
+cartRouter.get("/", async (req, res) => {
+  const data = await CartModel.find();
+  res.send(data);
+});
+
+cartRouter.post("/", async (req, res) => {
+  const newItem = await new CartModel(req.body);
+  await newItem.save((err, data) => {
+    if (err) {
+      res
+        .status(500)
+        .send({ message: "Error occured while adding cart item to Database", data });
+    } else {
+      res
+        .status(201)
+        .send({ message: "Item added to cart successfully", data });
+    }
+  });
+});
+
+cartRouter.delete("/:itemid", async (req, res) => {
+  const id = req.params.itemid;
+  await CartModel.deleteOne({ id });
+  res.send("Deleted successfully");
+});
+
+cartRouter.put("/:itemid", async (req, res) => {
+  const id = req.params.itemid;
+  let { quantity } = req.body;
+  await CartModel.updateOne({ id }, { $set: { quantity } });
+  res.send("Updated successfully successfully");
+});
+
+module.exports = cartRouter;
