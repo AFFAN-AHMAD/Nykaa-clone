be823e9ad0c9d338090eb2c1aa141492630aeaac
aakarsh604 <aakarsh604@gmail.com>
2022-08-28 15:24:55 +0530

    added pincode check feature

--- frontend/src/store/cart/cart.types.js
+++ frontend/src/store/cart/cart.types.js
@@ -6,6 +6,10 @@ export const GET_ITEM_CART_LOADING = "getitemcart/loading"
 export const GET_ITEM_CART_SUCCESS = "getitemcart/success"
 export const GET_ITEM_CART_ERROR = "getitemcart/error"
 
+export const GET_SINGLE_ITEM_CART_LOADING = "getsingleitemcart/loading"
+export const GET_SINGLE_ITEM_CART_SUCCESS = "getsingleitemcart/success"
+export const GET_SINGLE_ITEM_CART_ERROR = "getsingleitemcart/error"
+
 export const REMOVE_FROM_CART_LOADING = "removefromcart/loading";
 export const REMOVE_FROM_CART_SUCCESS = "removefromcart/success";
 export const REMOVE_FROM_CART_ERROR = "removefromcart/error";
