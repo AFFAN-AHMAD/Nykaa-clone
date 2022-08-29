88661809aca9867796a1ad2bb1f1f369b3c0c442
Affan Ahmad <affanahmad2797@gmail.com>
2022-08-29 12:42:25 +0500

    code

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -35,62 +35,82 @@ const navigate = useNavigate()
     });
   };
   return (
-    <div style={{ backgroundColor: "#f3f3f3" }}>
-      <div style={{ backgroundColor: "#f3f3f3" }}>
-        <hr />
-      </div>
-      <div className={styles.sc1}>
-        <img
-          style={{ height: "17px", margin: "4vh 0vh 0vh 4vh" }}
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyVhNLTUnAPBQYMepezLlKgTmXwNHlHnKTdlFYGlgW56q9V9lJ3cpSiYLBrRgkRRuFSd0&usqp=CAU"
-          alt=""
-          onClick={()=>navigate("/otp")}
-        />
-        <h1>LOGIN</h1>
+		<div>
+			<div>
+				<hr />
+			</div>
+			<div style={{paddingBottom:"60px"}}>
+				<div className={styles.sc1}>
+					<img
+						style={{ height: "17px", margin: "4vh 0vh 0vh 4vh" }}
+						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyVhNLTUnAPBQYMepezLlKgTmXwNHlHnKTdlFYGlgW56q9V9lJ3cpSiYLBrRgkRRuFSd0&usqp=CAU"
+						alt=""
+						onClick={() => navigate("/otp")}
+					/>
+					<h1>LOGIN</h1>
 
-        <hr style={{ margin: "2vh 2vh 0vh 2vh" }} />
-        <form onSubmit={handleSubmit}>  
-          <input
-            style={{
-              border: "1px solid red",
-              height: "6vh",
-              margin: "5vh 0vh 0vh 4vh",
-              width: "85%",
-              textAlign: "center",
-              outline: "red",
-            }}
-            type="text"
-            name="email"
-            onChange={handleChange}
-            placeholder="Username"
-          />
-          <input  style={{
-              border: "1px solid red",
-              height: "6vh",
-              margin: "2vh 0vh 0vh 4vh",
-              width: "85%",
-              textAlign: "center",
-              outline: "red",
-            }} type="text" name="password" onChange={handleChange} placeholder="Password"/>
-          <br />
-          <br />
+					<hr style={{ margin: "2vh 2vh 0vh 2vh" }} />
+					<form onSubmit={handleSubmit}>
+						<input
+							style={{
+								border: "1px solid red",
+								height: "6vh",
+								margin: "5vh 0vh 0vh 4vh",
+								width: "85%",
+								textAlign: "center",
+								outline: "red",
+							}}
+							type="text"
+							name="email"
+							onChange={handleChange}
+							placeholder="Username"
+						/>
+						<input
+							style={{
+								border: "1px solid red",
+								height: "6vh",
+								margin: "2vh 0vh 0vh 4vh",
+								width: "85%",
+								textAlign: "center",
+								outline: "red",
+							}}
+							type="text"
+							name="password"
+							onChange={handleChange}
+							placeholder="Password"
+						/>
+						<br />
+						<br />
 
-          <input style={{
-              backgroundColor: "#fb2e79",
-              margin: "2vh 0vh 0vh 4vh",
-              color: "white",
-              width: "85%",
-              height: "6vh",
-              fontSize: "1.1rem",
-              fontWeight: "500",
-              borderRadius: "3px",
-            }} type="submit" />
+						<input
+							style={{
+								backgroundColor: "#fb2e79",
+								margin: "2vh 0vh 0vh 4vh",
+								color: "white",
+								width: "85%",
+								height: "6vh",
+								fontSize: "1.1rem",
+								fontWeight: "500",
+								borderRadius: "3px",
+							}}
+							type="submit"
+						/>
 
-        <Link to="/verifymail" style={{margin:"0vh 0vh 0vh 30vh",color:"blue",fontSize:"0.9rem"}}>Forgot password?</Link>
-        </form>
-      </div>
-    </div>
-  );
+						<Link
+							to="/verifymail"
+							style={{
+								margin: "0vh 0vh 0vh 30vh",
+								color: "blue",
+								fontSize: "0.9rem",
+							}}
+						>
+							Forgot password?
+						</Link>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
--- frontend/src/pages/Login/Verifymail.jsx
+++ frontend/src/pages/Login/Verifymail.jsx
@@ -22,45 +22,54 @@ const navigate = useNavigate()
     .catch((err)=>{console.log(err);alert(" hack krta hai")})
  }
   return (
-    
-<div style={{ backgroundColor: "#f3f3f3" }}>
-      <div style={{ backgroundColor: "#f3f3f3" }}>
-        <hr />
-      </div>
-      <div className={styles.sc1}>
-      <img
-          style={{ height: "17px", margin: "4vh 0vh 0vh 4vh" }}
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyVhNLTUnAPBQYMepezLlKgTmXwNHlHnKTdlFYGlgW56q9V9lJ3cpSiYLBrRgkRRuFSd0&usqp=CAU"
-          alt=""
-          onClick={()=>navigate("/login")}
-        />
-        <h1>Verify Email</h1>
-        <hr style={{ margin: "2vh 2vh 0vh 2vh" }} />
-<input 
-style={{
-  border: "1px solid red",
-  height: "6vh",
-  margin: "5vh 0vh 0vh 4vh",
-  width: "85%",
-  textAlign: "center",
-  outline: "red",
-}}
-placeholder='Confirm Email Address' type="email" name='email'  onChange={(e)=>setEmail(e.target.value)}></input>
-<br></br>
-<button style={{
-              backgroundColor: "#fb2e79",
-              margin: "2vh 0vh 0vh 4vh",
-              color: "white",
-              width: "85%",
-              height: "6vh",
-              fontSize: "1.1rem",
-              fontWeight: "500",
-              borderRadius: "3px",
-            }} onClick={handleSubmit} >verify</button>
-      </div>
-
-    </div>
-  )
+		<div style={{ backgroundColor: "#f3f3f3" }}>
+			<div style={{ backgroundColor: "#f3f3f3" }}>
+				<hr />
+			</div>
+			<div style={{ paddingBottom: "60px" }}>
+				<div className={styles.sc1}>
+					<img
+						style={{ height: "17px", margin: "4vh 0vh 0vh 4vh" }}
+						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyVhNLTUnAPBQYMepezLlKgTmXwNHlHnKTdlFYGlgW56q9V9lJ3cpSiYLBrRgkRRuFSd0&usqp=CAU"
+						alt=""
+						onClick={() => navigate("/login")}
+					/>
+					<h1>Verify Email</h1>
+					<hr style={{ margin: "2vh 2vh 0vh 2vh" }} />
+					<input
+						style={{
+							border: "1px solid red",
+							height: "6vh",
+							margin: "5vh 0vh 0vh 4vh",
+							width: "85%",
+							textAlign: "center",
+							outline: "red",
+						}}
+						placeholder="Confirm Email Address"
+						type="email"
+						name="email"
+						onChange={(e) => setEmail(e.target.value)}
+					></input>
+					<br></br>
+					<button
+						style={{
+							backgroundColor: "#fb2e79",
+							margin: "2vh 0vh 0vh 4vh",
+							color: "white",
+							width: "85%",
+							height: "6vh",
+							fontSize: "1.1rem",
+							fontWeight: "500",
+							borderRadius: "3px",
+						}}
+						onClick={handleSubmit}
+					>
+						verify
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Verifymail
